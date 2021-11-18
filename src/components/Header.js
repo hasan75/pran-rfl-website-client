3c449d57ab8650615d6a47062face8d3181e60cd
Hasan75 <hasanjab14@gmail.com>
2021-11-19 02:38:07 +0600

    project header and footer added

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../assets/css/header.css'
+import { Container, Nav, Navbar, Spinner } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+import logo from '../assets/images/logo.png'
+
+const Header = () => {
+    return (
+        <div className="bg-dark">
+            <Navbar className="navBar" variant="dark" expand="lg">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <img width="100px" height="50px" src={logo} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link activeClassName="current" as={NavLink} to="/home">
+                                 Home
+                            </Nav.Link>
+
+                            <Nav.Link activeClassName="current" as={NavLink} to="/products">
+                               Products
+                            </Nav.Link>
+                            <Nav.Link activeClassName="current" as={NavLink} to="/dashboard">
+                                 Dashboard
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            
+        </div>
+    );
+};
+
+export default Header;
